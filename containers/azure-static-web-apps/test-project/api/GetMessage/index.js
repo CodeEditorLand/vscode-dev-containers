003e9bc40a3f7d65b347e9c1c33e45840eb282ec
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-02-19 00:03:04 +0200

    squash!

--- containers/azure-static-web-apps/test-project/api/GetMessage/index.js
+++ containers/azure-static-web-apps/test-project/api/GetMessage/index.js
@@ -1,7 +1,7 @@
-module.exports = async function (context, req) {
-    context.res = {
-        body: {
-            text: "Hello from the API"
-        }
-    };
+module.exports = async (context, req) => {
+	context.res = {
+		body: {
+			text: "Hello from the API",
+		},
+	};
 };
